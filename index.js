91f801f357440cc645a6aa22d993b87344d6dd27
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-05-21 21:56:16 +0500

    update code

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 
 app.get('/',(req,res)=>{
     console.log("Get request is running")
+    res.send("Welcome to our home page")
 })
 
 app.get('/about',(req,res)=>{
     console.log("about page is running")
+    res.send("Welcome to our about page")
 })
 
 app.listen(5000,()=>{
